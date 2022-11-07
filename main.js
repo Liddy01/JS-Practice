257e517f7242855c0e79e4ae3a5629bb397878a0
Liddy01 <lydianenye@gmail.com>
2022-11-07 08:28:23 -0800

    Primitive data types

--- main.js
+++ main.js
@@ -19,3 +19,31 @@
 // goods = 400;
 // const surname = 'Stephen'
 // console.log(goods,surname);
+
+
+
+
+
+// Primitive data types (string, number, boolean, undefined, null)
+let name = 'Ariyan';
+console.log(typeof name);
+
+let surname = "Lydia"
+console.log(typeof surname);
+
+let age = 40;
+console.log(age);
+
+let isMarried = true;
+console.log(typeof isMarried);
+
+let colors;
+console.log(typeof colors);
+
+let myName = undefined;
+console.log(typeof myName);
+
+let selectColor = null;
+console.log(typeof selectColor);
+
+
