424ba64f443d05eb262d378647d84daa7d956e71
Liddy01 <lydianenye@gmail.com>
2022-11-07 08:27:41 -0800

    Primitive data types

--- main.js
+++ main.js
@@ -9,13 +9,13 @@
 
 
 
-// Uses of let and const
-let price = 250;
-const name = 'Ariyan'
-console.log(price,name);
+// // Uses of let and const
+// let price = 250;
+// const name = 'Ariyan'
+// console.log(price,name);
 
-// let can be reassigned but const can't
-let goods = 300;
-goods = 400;
-const surname = 'Stephen'
-console.log(goods,surname);
+// // let can be reassigned but const can't
+// let goods = 300;
+// goods = 400;
+// const surname = 'Stephen'
+// console.log(goods,surname);
