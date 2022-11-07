d3f523ef4649cca298f139da61caada3bc59adea
Liddy01 <lydianenye@gmail.com>
2022-11-07 13:05:30 -0800

    Arithmetic Operators

--- main.js
+++ main.js
@@ -190,3 +190,21 @@
 
 
 
+// JS Operators
+
+// Arithmetic Operators
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + num2); 
+console.log(num1 - num2); 
+console.log(num1 * num2); 
+console.log(num1 / num2); 
+console.log(num1 ** num2); 
+console.log(num1 % num2); 
+
+
+
+
+
+
