f88e0478feb4b97e754279c70d48dd1ea853e92f
Liddy01 <lydianenye@gmail.com>
2022-11-07 05:28:29 -0800

    Uses of let and const

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-// Variables
+// // Variables
 
-var name = 'Lydia';
-console.log(name);
+// var name = 'Lydia';
+// console.log(name);
 
-// variable name should be meaningful,cannot start with numbers, contain space or hyphen 
+// // variable name should be meaningful,cannot start with numbers, contain space or hyphen 
+
+
+
+
+
+// Uses of let and const
+let price = 250;
+const name = 'Ariyan'
+console.log(price,name);
+
+// let can be reassigned but const can't
+let goods = 300;
+goods = 400;
+const surname = 'Stephen'
+console.log(goods,surname);
