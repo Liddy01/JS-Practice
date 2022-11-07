9ddf1d6d12e74b2a2ab32c04fbcfce1ccebd19f9
Liddy01 <lydianenye@gmail.com>
2022-11-07 05:10:29 -0800

    Uses of Let and Const

--- main.js
+++ main.js
@@ -4,13 +4,3 @@ var name = 'Lydia';
 console.log(name);
 
 // variable name should be meaningful,cannot start with numbers, contain space or hyphen 
-
-
-
-
-
-
-// Uses of let and const
-let price = 250;
-const surname = 'Bless';
-console.log(price,name);
