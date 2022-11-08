1bfc0b2a4cf505ee1d0dafc7c3d3db035933bf89
Liddy01 <lydianenye@gmail.com>
2022-11-07 21:20:24 -0800

    chng

--- main.js
+++ main.js
@@ -248,31 +248,31 @@
 // Comparison Operator
 // < > <= >= === !==
 
-let price = 200;
+// let price = 200;
 
-console.log(price > 300);
-console.log(price >= 200);
-console.log(price < 100);
-console.log(price <= 500);
-console.log(price === 500);
-console.log(price !== 500);
-
-
+// console.log(price > 300);
+// console.log(price >= 200);
+// console.log(price < 100);
+// console.log(price <= 500);
+// console.log(price === 500);
+// console.log(price !== 500);
 
 
 
 
-// Logical Operator
-// and(&&), or(||), not(!)
 
-let goods = 10;
-console.log(goods > 5 && goods < 15);
-// true && true
 
-console.log(goods > 50 || goods < 20);
+// // Logical Operator
+// // and(&&), or(||), not(!)
 
-console.log(!(goods > 50));
-console.log(!(goods > 5));
+// let goods = 10;
+// console.log(goods > 5 && goods < 15);
+// // true && true
+
+// console.log(goods > 50 || goods < 20);
+
+// console.log(!(goods > 50));
+// console.log(!(goods > 5));
 
 
 
