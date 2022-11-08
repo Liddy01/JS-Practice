7ae5b0db3329e20c1e120c56322647746bceda5d
Liddy01 <lydianenye@gmail.com>
2022-11-07 20:44:40 -0800

    Assignment Operators

--- main.js
+++ main.js
@@ -193,15 +193,54 @@
 // JS Operators
 
 // Arithmetic Operators
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-console.log(num1 + num2); 
-console.log(num1 - num2); 
-console.log(num1 * num2); 
-console.log(num1 / num2); 
-console.log(num1 ** num2); 
-console.log(num1 % num2); 
+// console.log(num1 + num2); 
+// console.log(num1 - num2); 
+// console.log(num1 * num2); 
+// console.log(num1 / num2); 
+// console.log(num1 ** num2); 
+// console.log(num1 % num2); 
+
+
+
+
+
+// Assignment Operators
+let num1 = 10;
+let num2 = num1;
+console.log(num2);
+console.log(num1,num2);
+
+num1 = num1 + 5;
+console.log(num1);
+
+num1 +=5; 
+console.log(num1);
+
+num1 = num1 - 5;
+console.log(num1);
+
+num1 -= 5;
+console.log(num1);
+
+num1 = num1 * 5;
+num1 *= 5;
+
+num1 = num1 + 1;
+num1 += 1;
+num1++;
+console.log(num1);
+
+num1 = num1 - 1;
+num1 -= 1;
+num1--;
+console.log(num1);
+
+
+
+
 
 
 
