baaf9b32d814a62f209416b1a25defae34770ff4
Liddy01 <lydianenye@gmail.com>
2022-11-07 20:45:08 -0800

    chng

--- main.js
+++ main.js
@@ -208,35 +208,35 @@
 
 
 // Assignment Operators
-let num1 = 10;
-let num2 = num1;
-console.log(num2);
-console.log(num1,num2);
+// let num1 = 10;
+// let num2 = num1;
+// console.log(num2);
+// console.log(num1,num2);
 
-num1 = num1 + 5;
-console.log(num1);
+// num1 = num1 + 5;
+// console.log(num1);
 
-num1 +=5; 
-console.log(num1);
+// num1 +=5; 
+// console.log(num1);
 
-num1 = num1 - 5;
-console.log(num1);
+// num1 = num1 - 5;
+// console.log(num1);
 
-num1 -= 5;
-console.log(num1);
+// num1 -= 5;
+// console.log(num1);
 
-num1 = num1 * 5;
-num1 *= 5;
+// num1 = num1 * 5;
+// num1 *= 5;
 
-num1 = num1 + 1;
-num1 += 1;
-num1++;
-console.log(num1);
+// num1 = num1 + 1;
+// num1 += 1;
+// num1++;
+// console.log(num1);
 
-num1 = num1 - 1;
-num1 -= 1;
-num1--;
-console.log(num1);
+// num1 = num1 - 1;
+// num1 -= 1;
+// num1--;
+// console.log(num1);
 
 
 
