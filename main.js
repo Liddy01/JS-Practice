3a262f7b50a60b04bd6120ed6d9f4f0b9487ea58
Liddy01 <lydianenye@gmail.com>
2022-11-08 20:53:07 -0800

    loop cont

--- main.js
+++ main.js
@@ -579,92 +579,133 @@
 // Loops
 // For loop
 
-console.log('Hi my name is Ariyan');
-console.log('Hi my name is Ariyan');
-console.log('Hi my name is Ariyan');
-console.log('Hi my name is Ariyan');
+// console.log('Hi my name is Ariyan');
+// console.log('Hi my name is Ariyan');
+// console.log('Hi my name is Ariyan');
+// console.log('Hi my name is Ariyan');
 
-for(let index = 1; index <= 5; index++){
-    console.log('Hi I am Jovita',index);
-}
+// for(let index = 1; index <= 5; index++){
+//     console.log('Hi I am Jovita',index);
+// }
 
-for(let index = 1; index <= 3; index++){
-    console.log('goodbye everyone', index)
-}
+// for(let index = 1; index <= 3; index++){
+//     console.log('goodbye everyone', index)
+// }
 
-for(let index = 5; index >= 1; index--){
-    console.log('Hi I am Jovita',index);
-}
-
-
-
-
-// While loop
-let index = 1;
-while(index <= 5){
-    console.log('Hi Steve', index)
-    index++;
-}
-
-let box = 3;
-while(box >= 1){
-    console.log('Hi Steve', box)
-    box--;
-}
+// for(let index = 5; index >= 1; index--){
+//     console.log('Hi I am Jovita',index);
+// }
 
 
 
 
-// Do while loop
-let service = 1;
-do{
-    console.log('Hey you',service);
-    service++;
-} while(service <= 5);
+// // While loop
+// let index = 1;
+// while(index <= 5){
+//     console.log('Hi Steve', index)
+//     index++;
+// }
+
+// let box = 3;
+// while(box >= 1){
+//     console.log('Hi Steve', box)
+//     box--;
+// }
 
 
 
 
-// For in loop
-const objs = {
-    name : 'Grace',
-    age : 10,
-}
-
-for (let key in objs){
-    console.log(key, objs[key])
-}
-
-
-let numbers = [1,2,3,4,5];
-console.log(numbers);
-
-for (let index in numbers){
-    console.log(index, );
-}
-
-let counts = [1,2,3,4,5];
-console.log(counts);
-
-for (let index in counts){
-    console.log(index,counts[index]);
-}
+// // Do while loop
+// let service = 1;
+// do{
+//     console.log('Hey you',service);
+//     service++;
+// } while(service <= 5);
 
 
 
-// For of loop
-let digits = [1,2,3,4,5,6];
-console.log(digits);
 
-for (let index in digits){
-    console.log(index);
-}
+// // For in loop
+// const objs = {
+//     name : 'Grace',
+//     age : 10,
+// }
 
-for (let num of digits){
-    console.log(num);
-}
+// for (let key in objs){
+//     console.log(key, objs[key])
+// }
+
+
+// let numbers = [1,2,3,4,5];
+// console.log(numbers);
+
+// for (let index in numbers){
+//     console.log(index, );
+// }
+
+// let counts = [1,2,3,4,5];
+// console.log(counts);
+
+// for (let index in counts){
+//     console.log(index,counts[index]);
+// }
+
+
+
+// // For of loop
+// let digits = [1,2,3,4,5,6];
+// console.log(digits);
+
+// for (let index in digits){
+//     console.log(index);
+// }
+
+// for (let num of digits){
+//     console.log(num);
+// }
+
+
+
+
 
 
 
 // Break & Continue
 
+for (let index = 1; index <=20; index++){
+    console.log(index);
+
+    if (index === 10){
+        break;
+    }
+
+}
+
+
+for (let index = 1; index <=20; index++){
+    if (index === 10){
+        break;
+    }
+    console.log(index);
+
+}
+
+for (let index = 1; index <=20; index++){
+    if (index === 10){
+        continue;
+    }
+    console.log(index);
+
+}
+
+
+
+
+// Infinite loop
+for (let index = 1; ; index++){
+    console.log(index);
+}
+
+
+
+// Nested loop
