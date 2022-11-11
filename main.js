bcf6e28940d3d2a9c0c2fb7d3017912135badb49
Liddy01 <lydianenye@gmail.com>
2022-11-11 09:43:48 -0800

    Arrow Function

--- main.js
+++ main.js
@@ -1207,16 +1207,39 @@
 // Function Expression
 
 // Default Parametres
-function calcSum(number1,number2){
-        let sum = number1 + number2;
-    return sum; 
-    }
+// function calcSum(number1,number2){
+//         let sum = number1 + number2;
+//     return sum; 
+//     }
     
-    console.log(calcSum(10,20));
+//     console.log(calcSum(10,20));
 
- // Function Expression
-    const calSumExp = function(number1,number2){
-        let sum = number1 + number2;
-        return sum;
-    }
-    console.log(calSumExp(50,60));
+//  // Function Expression
+//     const calSumExp = function(number1,number2){
+//         let sum = number1 + number2;
+//         return sum;
+//     }
+//     console.log(calSumExp(50,60));
+
+
+
+
+
+
+
+
+// Arrow Function
+const calSumArr = (number1,number2) => number1 + number2;
+console.log(calSumArr(40,30));
+
+
+
+// Function Expression
+const mulByTwo = function(number1){
+    return number1 * 2;
+}
+console.log(mulByTwo(6));
+
+// Arrow Function
+const mulByTwoArr = (number1) => number1 *2;
+console.log(mulByTwoArr(6));
